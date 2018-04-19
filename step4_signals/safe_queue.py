3b6ca94fa944a0de42db710f2c5f33da26caa21c
Pierrick Rassat <rassat.pierrick@gmail.com>
2018-04-19 22:34:11 +0200

    Improve step5 code sample

--- step4_signals/safe_queue.py
+++ step4_signals/safe_queue.py
@@ -5,14 +5,13 @@ import pika
 class Step4Consumer:
     def start(self):
         self._connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        self._channel = self._connection.channel()
-        self._channel.queue_declare(queue='safe_hello')
-        self._channel.basic_consume(queue='safe_hello', no_ack=True, consumer_callback=self.consume)
-        self._channel.add_on_cancel_callback(self.on_cancel)
-        self._channel.start_consuming()
+        channel = self._connection.channel()
+        channel.queue_declare(queue='safe_hello')
+        channel.basic_consume(queue='safe_hello', no_ack=True, consumer_callback=self.consume)
+        channel.add_on_cancel_callback(self.on_cancel)
+        channel.start_consuming()
 
     def on_cancel(self, method_frame):
-        self._channel.close()
         self._connection.close()
         self.start()
 
